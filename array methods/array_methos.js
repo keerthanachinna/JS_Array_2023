9cf9ef1873fe9a6f2693a70c9ac55164e706de13
keerthanac <keerthanachinna97@gmail.com>
2023-06-19 00:52:59 +0530

    array 16 importan methods and string concat types

---
+++ array methods/array_methos.js
@@ -0,0 +1,45 @@
+toString()
+join()
+concat()
+splice()
+slice();
+indexOf()
+lastIndexOf()
+flat()
+// iteration
+forEach()
+map()
+fliter()
+reduce()
+//////////////
+every()
+some()
+find()
+findIndex()
+sort()
+ delete()
+
+fruits.length
+ pop()
+ push()
+ shift()
+ unshift()
+
+
+fruits.reverse();
+keys();
+from();
+entries();
+includes();
+
+const cars = ["Volvo", "BMW"];   // Allowed
+const cars = ["Volvo", "BMW"];   // Not allowed
+var cars = ["Volvo", "BMW"];     // Not allowed
+cars = ["Volvo", "BMW"];         // Not allowed
+
+{
+  const cars = ["Volvo", "BMW"]; // Allowed
+  const cars = ["Volvo", "BMW"]; // Not allowed
+  var cars = ["Volvo", "BMW"];   // Not allowed
+  cars = ["Volvo", "BMW"];       // Not allowed
+}
---
+++ combine_strings.js
@@ -0,0 +1,12 @@
+The+ operator
+const str='Hello'+'World';
+//Hello world
+Array.join()
+['Hello',' ','World'].join('')
+
+//Hello world
+String.concat()
+const str1='Hello'
+const str2=str1.concat(' ','World');
+
+//Hello world
